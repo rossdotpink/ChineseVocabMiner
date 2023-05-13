3eaec66a7de57b7600e77f4af479b6a85f31ea5c
Ross Wilson <ross.wilson@ross.pink>
2023-05-13 21:47:18 +0200

    Got recursivelyPrint working

--- src/main/java/dev/rosswilson/chinesevocabminer/CharacterTreeBuilder.java
+++ src/main/java/dev/rosswilson/chinesevocabminer/CharacterTreeBuilder.java
@@ -1,7 +1,6 @@
 package dev.rosswilson.chinesevocabminer;
 
 import java.util.*;
-import java.util.stream.*;
 
 public class CharacterTreeBuilder {
 
@@ -10,10 +9,10 @@ public class CharacterTreeBuilder {
     public CharacterTreeBuilder() {}
 
     public CharacterTree<Character, DictionaryEntry> buildCharacterTree(List<DictionaryEntry> list) {
-        tree = new CharacterTree<Character, DictionaryEntry>('r');
+        tree = new CharacterTree<>('r');
         tree.getRoot().setData(new DictionaryEntry(List.of('r'), List.of('r'), "Root", List.of("Root node")));
         list.stream().forEach(s -> {
-//            addEntry(tree.getRoot(), s.getSimplified().iterator(), s);
+            addEntry(tree.getRoot(), s.getSimplified().iterator(), s);
             addEntry(tree.getRoot(), s.getTraditional().iterator(), s);
         });
         return tree;
@@ -24,7 +23,6 @@ public class CharacterTreeBuilder {
                          DictionaryEntry entry) {
         // Advance the character
         Character character = characterIterator.next();
-//        CharacterTree.Node<Character, DictionaryEntry> nextNode;
 
         // If current character exists and we have more characters
         if (!Objects.isNull(node.getChild(character)) && characterIterator.hasNext()) {
@@ -32,9 +30,7 @@ public class CharacterTreeBuilder {
         }
 
         // If current character exists and we have no more characters
-//        if (!Objects.isNull(node.getChild(character)) && !characterIterator.hasNext()) {
-//            addEntry(node.getChild(character), characterIterator, entry);
-//        }
+        // ...
 
         // If current character does not exist, and we have more characters
         if (Objects.isNull(node.getChild(character)) && characterIterator.hasNext()) {
@@ -42,12 +38,6 @@ public class CharacterTreeBuilder {
             newNode.setId(character);
             node.addChild(newNode);
             addEntry(node.getChild(character), characterIterator, entry);
-//            System.out.println("Adding '" + character + "' as child of '" + node.getId() +"'");
-//            try {
-//                Thread.sleep(100);
-//            } catch (Exception e) {
-//
-//            }
         }
 
         // If current character does not exist, and we don't have more characters
@@ -56,7 +46,6 @@ public class CharacterTreeBuilder {
             newNode.setId(character);
             newNode.setData(entry);
             node.addChild(newNode);
-            return;
         }
 
     }
--- src/test/java/dev/rosswilson/chinesevocabminer/CharacterTreeBuilderTest.java
+++ src/test/java/dev/rosswilson/chinesevocabminer/CharacterTreeBuilderTest.java
@@ -2,11 +2,6 @@ package dev.rosswilson.chinesevocabminer;
 
 import org.junit.jupiter.api.*;
 
-import java.net.*;
-import java.util.*;
-
-import static org.junit.jupiter.api.Assertions.*;
-
 class CharacterTreeBuilderTest {
 
     CharacterTreeBuilder builder;
@@ -20,39 +15,29 @@ class CharacterTreeBuilderTest {
         tree = builder.buildCharacterTree(loader.getDictionaryEntryStream(
                 "/Users/rosswilson/IdeaProjects/ChineseVocabMiner/src/main/resources/cedict_ts.u8"));
         recursivelyPrint(tree.getRoot(), 0);
-//        for (CharacterTree.Node<Character, DictionaryEntry> node : tree.getRoot().getChildren()) {
-//            String def = Objects.isNull(node.getData()) ? "noDef" : node.getData().getTraditional().toString();
-//            System.out.println(node.getId() + " : " + def);
-//            if (!node.getChildren().isEmpty()) {
-//                for (CharacterTree.Node<Character, DictionaryEntry> innerNode : tree.getRoot().getChildren()) {
-//                    String innerDef = Objects.isNull(innerNode.getData()) ? "noDef" : innerNode.getData().getTraditional().toString();
-//                    System.out.println("- " + innerNode.getId() + " : " + innerDef);
-//                    if (!innerNode.getChildren().isEmpty()) {
-//
-//                        for (CharacterTree.Node<Character, DictionaryEntry> innerInnerNode : tree.getRoot().getChildren()) {
-//                            String innerInnerDef = Objects.isNull(innerInnerNode.getData()) ? "noDef" : innerInnerNode.getData().getTraditional().toString();
-//                            System.out.println("-- " + innerInnerNode.getId() + " : " + innerInnerDef);
-//                            Thread.sleep(200);
-//                        }
-//                    }
-//                }
-//            }
-//        }
     }
 
     void recursivelyPrint(CharacterTree.Node<Character, DictionaryEntry> rootNode, int indentation) throws InterruptedException {
-        Thread.sleep(200);
+        Thread.sleep(50);
         for (int i = 1; i < indentation; i++) {
             System.out.print('-');
         }
+        indentation++;
 //        System.out.println(rootNode.getId() + rootNode.getData().getTraditional().toString());
-        System.out.println(rootNode.getId());
+        System.out.print(rootNode.getId());
 
-        int loopIndentation = indentation++;
+        if (rootNode.getChildren().isEmpty()) {
+            System.out.println(" : " + rootNode.getData().getDefinition().get(0));
+            return;
+        }
+        System.out.println();
+
         for (CharacterTree.Node<Character, DictionaryEntry> childNode : rootNode.getChildren()) {
 
-            recursivelyPrint(childNode, loopIndentation++);
+            recursivelyPrint(childNode, indentation);
         }
+
+
 
 
     }
